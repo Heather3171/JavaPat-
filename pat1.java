fb314faa96f62b6d6b0f1c3c0289d939ef211dce
Heather3171 <119729788+Heather3171@users.noreply.github.com>
2023-04-20 22:14:56 +0530

    Add files via upload

---
+++ pat1.java
@@ -0,0 +1,22 @@
+package begin;
+import java.util.*;
+public class pat1 {
+
+	public pat1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.print(5^2);
+		while(n>0) {
+			
+			
+			
+		}
+	}
+	}
+
+
